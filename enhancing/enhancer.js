2723699ff81e3211d4ca35aa6fb9474ae1854cfc
jvgiv <jvgiv12@gmail.com>
2019-05-13 18:44:31 -0400

    Functions set up, testing 2/3 complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,37 @@ module.exports = {
   get,
 };
 
+const test = {
+  name: 'sword', 
+  enhancement: 6,
+  durability: 80
+}
+
 function succeed(item) {
+  if (item.enhancement === 20) {
+    return item
+  } else if(item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability < 5 ? item.durability = item.durability - item.durability : item.durability = item.durability - 5;
+  } else if (item.enhancement >= 15 && item.enhancement <= 16){
+    item.durability > 10 ? item.durability = item.durability - 10 : item.durability = item.durability - item.durability
+  } else if (item.enhancement > 16) {
+    (item.enhancement = item.enhancement - 1) && (item.durability > 10 ? item.durability = item.durability - 10 : item.durability = item.durability - item.durability)
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.durability < 100) {
+    item.durability = 100 
+  }
+  return {...item}
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,63 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+
+    describe('repair method', () => {
+        it('repair should return durability to 100', () => {
+
+            const repairTest = enhancer.repair({name: 'Dolphin', enhancement: 17, durability: 69})
+            
+            expect(repairTest).toBeDefined();
+            
+            expect(repairTest).toStrictEqual({name: 'Dolphin', enhancement: 17, durability: 100})
+        })
+    })
+        
+        
+        describe('succeed method', () => {
+            it('succeed should take an item and return a new item with enhancement + 1', () => {
+                const succeedTest = enhancer.succeed({name: 'Dolphin', enhancement: 17, durability: 69})
+
+                expect(succeedTest).toBeDefined();
+
+                expect(succeedTest).toStrictEqual({name: 'Dolphin', enhancement: 18, durability: 69})
+            })
+            
+            it('succeed should take an item with 20 enhancement and return it with 20 enhancement still', () => {
+                const twentyMaxEnhancement = enhancer.succeed({name: '20 Max', enhancement: 20, durability: 69})
+
+                expect(twentyMaxEnhancement).toBeDefined();
+
+                expect(twentyMaxEnhancement).toStrictEqual({name: '20 Max', enhancement: 20, durability: 69})
+                
+            })
+        })
+        
+        
+        describe('fail method ', () => {
+            it('enhancement less than 15? decrease durability by 5', () => {
+
+            })
+
+
+            
+            it('enhancement is either 15 or 16? reduce durability by 10', () =>{
+
+            })
+
+            
+
+            it('enhancement greater than 16? decrease enhancement by 1 and dura by 10', () => {
+
+            })
+        })
+        
+        
+        // describe('', () => {
+            
+            // })
+            
+            
+            
+        })
