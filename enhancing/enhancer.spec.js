ac281e92445d9a0754d28d11c75d350da0259f73
jvgiv <jvgiv12@gmail.com>
2019-05-13 18:54:58 -0400

    MVP? I think so

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -37,19 +37,52 @@ describe('enhancer.js', () => {
         
         describe('fail method ', () => {
             it('enhancement less than 15? decrease durability by 5', () => {
+                const fiveDecrease = enhancer.fail({name: 'Dolphin', enhancement: 12, durability: 69})
 
+                expect(fiveDecrease).toBeDefined();
+
+                expect(fiveDecrease).toStrictEqual({name: 'Dolphin', enhancement: 12, durability: 64})
             })
 
+            it('enhancement less than 15? decrease durability by 5, but not negative', () => {
+                const fiveDecreaseNotNeg = enhancer.fail({name: 'Dolphin', enhancement: 12, durability: 3})
+
+                expect(fiveDecreaseNotNeg).toBeDefined();
+
+                expect(fiveDecreaseNotNeg).toStrictEqual({name: 'Dolphin', enhancement: 12, durability: 0})
+            })
 
             
             it('enhancement is either 15 or 16? reduce durability by 10', () =>{
+                const tenDecrease = enhancer.fail({name: 'Dolphin', enhancement: 15, durability: 80})
 
+                expect(tenDecrease).toBeDefined();
+
+                expect(tenDecrease).toStrictEqual({name: 'Dolphin', enhancement: 15, durability: 70})
             })
 
-            
+            it('enhancement is either 15 or 16? reduce durability by 10, but not negative', () =>{
+                const tenDecreaseNotNeg = enhancer.fail({name: 'Dolphin', enhancement: 15, durability: 5})
+
+                expect(tenDecreaseNotNeg).toBeDefined();
+
+                expect(tenDecreaseNotNeg).toStrictEqual({name: 'Dolphin', enhancement: 15, durability: 0})
+            })
 
             it('enhancement greater than 16? decrease enhancement by 1 and dura by 10', () => {
+                const decEnAndDura = enhancer.fail({name: 'Dolphin', enhancement: 19, durability: 55})
 
+                expect(decEnAndDura).toBeDefined();
+
+                expect(decEnAndDura).toStrictEqual({name: 'Dolphin', enhancement: 18, durability: 45})
+            })
+
+            it('enhancement greater than 16? decrease enhancement by 1 and dura by 10, but not negative', () => {
+                const decEnAndDuraNotNeg = enhancer.fail({name: 'Dolphin', enhancement: 19, durability: 4})
+
+                expect(decEnAndDuraNotNeg).toBeDefined();
+
+                expect(decEnAndDuraNotNeg).toStrictEqual({name: 'Dolphin', enhancement: 18, durability: 0})
             })
         })
         
